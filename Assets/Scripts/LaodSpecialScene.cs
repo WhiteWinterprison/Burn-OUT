69dcd029158ce07e44afd9230f4cb65b086bd1ba
Isabel <isabelbartelmus@gmx.de>
2021-12-09 13:29:10 +0100

    Merge New Folder Struct

---
+++ Assets/Scripts/LaodSpecialScene.cs
@@ -0,0 +1,39 @@
+//Isabel because i dont get Jane script to work
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaodSpecialScene : MonoBehaviour
+{
+ [SerializeField]
+ private int RayCast;
+ [SerializeField]
+ private int ImageTracking;
+
+ [SerializeField]
+ private int UI;
+
+ [SerializeField]
+ private int Feet;
+     public void LoadRay()
+    {
+        SceneManager.LoadScene(RayCast);
+    }
+
+     public void LoadUi()
+    {
+        SceneManager.LoadScene(UI);
+    }
+
+     public void LoadImage()
+    {
+        SceneManager.LoadScene(ImageTracking);
+    }
+
+     public void LoadFeet()
+    {
+        SceneManager.LoadScene(Feet);
+    }
+
+}
