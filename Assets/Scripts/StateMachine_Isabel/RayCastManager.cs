524563e6e458edab48ebf035c9f07dfd8c6a288e
Isabel <isabelbartelmus@gmx.de>
2021-12-11 01:59:39 +0100

    Update Ray CastManager

--- Assets/Scripts/StateMachine_Isabel/RayCastManager.cs
+++ Assets/Scripts/StateMachine_Isabel/RayCastManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class RayCastManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    IState currentState;
+    #region GameSates
+   public GameStartedState GameStarted = new GameStartedState();
+
+   public RunGameState RunGame =new RunGameState();
+   
+   #endregion
     void Start()
     {
         
