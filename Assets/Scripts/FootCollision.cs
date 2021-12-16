375920c2ea66225e416898f40120d95406f9d891
marjeusw <marje.harms@web.de>
2021-12-16 01:16:05 +0100

    added collider script from newest project
    - add collider script from main branch
    - still needs the game over screen and every new version of ui (right now just gets back to the home screen when colliding with goo instead of stone)

--- Assets/Scripts/FootCollision.cs
+++ Assets/Scripts/FootCollision.cs
@@ -1,11 +1,43 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class FootCollision : MonoBehaviour
+//{
+//    public imageTracking tracking;
+//    bool canDie = false;
+
+//    void OnCollisionEnter(Collision collisioninfo)
+//    {
+//        if (collisioninfo.collider.tag == "Stone") //maybe coroutine so player cant die when entering collision (think its bc it only on enter and then back to dying)
+//        {
+//            canDie = false;
+//        }
+//        else
+//        {
+//            canDie = true;
+//        }
+
+//        if (collisioninfo.collider.tag == "Goo" && canDie == true)
+//        {
+//            tracking.enabled = false;
+//            FindObjectOfType<GameManager>().EndGame();
+
+//        }
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FootCollision : MonoBehaviour
 {
     public imageTracking tracking;
     bool canDie = false;
+    [SerializeField]
+    private int EndGame;
 
     void OnCollisionEnter(Collision collisioninfo)
     {
@@ -13,15 +45,31 @@ public class FootCollision : MonoBehaviour
         {
             canDie = false;
         }
-        else
-        {
-            canDie = true;
-        }
+        //else
+        //{
+        //    canDie = true;
+        //}
 
         if (collisioninfo.collider.tag == "Goo" && canDie == true)
         {
             tracking.enabled = false;
-            FindObjectOfType<GameManager>().EndGame();
+            SceneManager.LoadScene(EndGame);
+            //FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+
+    void OnCollisionExit(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Stone") //maybe coroutine so player cant die when entering collision (think its bc it only on enter and then back to dying)
+        {
+            canDie = true;
+        }
+
+        if (collisioninfo.collider.tag == "Goo")
+        {
+
+            //FindObjectOfType<GameManager>().EndGame();
 
         }
     }
