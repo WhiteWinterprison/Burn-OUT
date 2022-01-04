9ce8cdaf2a3e591cf092b1c62a64e1656f97188f
Isabel <isabelbartelmus@gmx.de>
2022-01-05 00:09:25 +0100

    Add Menue scene
    add Manue back
    add new material

--- Assets/Scripts/PlatformSapwning/StoneManager.cs
+++ Assets/Scripts/PlatformSapwning/StoneManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class StoneManager : MonoBehaviour
 {
     StoneStates currentState;
     
     public  StonePlacingState StonePlacing = new StonePlacingState();
+    public StoneIdleState StoneIdle = new StoneIdleState();
+    public StonePlacedState StonePlaced = new StonePlacedState();
+
 
     void Start()
     {
---
+++ Assets/Scripts/PlatformSapwning/StonePlacedState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StonePlacedState : StoneStates
+{
+    public override void enter (StoneManager gameState)
+    {
+
+    }
+     public override void react (StoneManager gameState)
+    {
+
+    }
+     public override void exit (StoneManager gameState)
+    {
+
+    }
+}
