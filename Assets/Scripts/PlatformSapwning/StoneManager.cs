292bd3edffd30ea7baf26f31e1946f5c5751d23b
Isabel <isabelbartelmus@gmx.de>
2022-01-04 18:19:31 +0100

    Update Ui Finish

---
+++ Assets/Scripts/PlatformSapwning/StoneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneManager : MonoBehaviour
+{
+    StoneStates currentState;
+    
+    public  StonePlacingState StonePlacing = new StonePlacingState();
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
