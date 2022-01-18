c8be9b62472a4cb2cef800c3f4fd368f7e9df08e
maneji <jane-kim.quoc@stud.h-da.de>
2022-01-18 12:50:20 +0100

    STateMachine Update

--- Assets/Scripts/WinningStateMachine/ColAnimState.cs
+++ Assets/Scripts/WinningStateMachine/ColAnimState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColAnimState : WState
+public class ColAnimState : WState  // collision animation state 
 {
 
     public override void enter(WinningManager gameState)
@@ -11,20 +11,21 @@ public class ColAnimState : WState
     }
     public override void react(WinningManager gameState)
     {
-        gameState.switchState(gameState.DeadState);
+        gameState.switchState(gameState.Dead);
     }
     public override void exit(WinningManager gameState)
     {
 
         // Start is called before the first frame update
         void Start()
-    {
-        
-    }
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
