2e268e37ae3460d30adf2d21177f95cf869579b8
marjeusw <marje.harms@web.de>
2022-01-24 02:14:18 +0100

    Bubble Effect
    - add image of bubble effect
    - included image on canvas with some transparency
    - add bubble icon
    - add Bubble script (nothing except comment on what it should do in there yet)

---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+
+    //turn on bubble effect for coroutine nr of seconds; then turn off again and wait for coroutine nr of seconds to turn on again
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
