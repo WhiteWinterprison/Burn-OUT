fcfa6ebd41ff247be12d8aa4329fdf730a1513e6
marjeusw <marje.harms@web.de>
2022-01-24 14:16:23 +0100

    Create SceneSpawner
    - add StateMachine for Scene Spawning
    - create spawning Prefab

---
+++ Assets/Scripts/SceneSpawning/LManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LManager : MonoBehaviour
+{
+    //------------------------Variablen------------------------
+    public bool Fun;
+    public Text DebugLog;
+    //------------------------States------------------------
+    LState currentState;
+    public LIdleState IdleState = new LIdleState();
+    public LSpawnState SpawnState = new LSpawnState();
+
+    //------------------------Spawn------------------------
+    public GameObject Level;
+
+    [SerializeField]
+    private GameObject cursor;
+    //public GameObject Cursorr;
+    [HideInInspector]
+    public bool PlayAreaSpawned = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = IdleState;
+        currentState.enter(this);
+
+        cursor = FindObjectOfType<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.react(this);
+    }
+
+    public void switchState(LState nextState)
+    {
+        currentState.exit(this);
+        currentState = nextState;
+        nextState.enter(this);
+
+    }
+
+    public void Spawner() //manages what is spawned when
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            //check if Game Field is already spawned if not go on
+            DebugLog.text = "Touched";
+
+            //Game Field
+
+            GameObject obj = Instantiate(Level, cursor.transform.position, cursor.transform.rotation);
+
+            DebugLog.text = "Spawned";
+            //Set SpawnedTrue so only one set of enviroment can be spawned
+            PlayAreaSpawned = true;
+
+        }
+    }
+
+    public void EnableCursor()
+    {
+        cursor.SetActive(true);
+    }
+    public void DisableCursor()
+    {
+        cursor.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SceneSpawning/LSpawnState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSpawnState : LState
+{
+    public override void enter(LManager gameState)
+    {
+        gameState.DisableCursor();
+        gameState.DebugLog.text = "Spawned";
+    }
+    public override void react(LManager gameState)
+    {
+
+    }
+    public override void exit(LManager gameState)
+    {
+
+    }
+}
