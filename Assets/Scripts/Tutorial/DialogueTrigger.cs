51f4badf2d62051017b1fba75294bff9b681f0c4
Isabel <isabelbartelmus@gmx.de>
2022-02-09 23:17:46 +0100

    tutorial and leaf menu icons
    - add tutorial (text without image of marvin)
    - add leaf icon buttons for the main menu (and stem)
    - add tutorial1.0 scene with tutorial
    - made marje image tracking scene unusable by trying to recreate everything so now that one does basically nothing

---
+++ Assets/Scripts/Tutorial/DialogueTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject bubble;
+    public GameObject leaf;
+    public GameObject abyss;
+    public GameObject roomScann;
+
+
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void TurnOff() //turn off button and all the things to tap on screen (bubble,leafs,spawn abyss) //guess itll be easier to just make a state 
+    {
+
+        bubble.SetActive(false);
+        leaf.SetActive(false); //make a reference to the button component and set it inactive instead
+        abyss.SetActive(false);//can't make the abyss not work its so hard to break this mechanic damn probs to you izzy D:
+        roomScann.SetActive(false);
+    }
+}
